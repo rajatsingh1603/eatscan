d0393f98aee182e979d9d909f63a01629c69f738
rajat230522 <106083365+rajat230522@users.noreply.github.com>
2023-05-20 22:23:59 +0530

    add subtract order

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,48 @@
-import food from "../utils/Images/food.jpg"
-import '../components/menu.css'
-const Card = ()=>{
-    return (
-        <>
-        <div className="card-container">
-            <div className="card-img"><img src={food}/></div>
-            <div className="card-name-add-remove">
-                <div className="card-inner">
-                <h2>Biryani</h2>
-                <div className="card-add-remove">
-                <span className="plus">+</span>
-               <input type="text" value="1" className="card-input" />
-                <span className="minus">-</span>
-                </div>
-                </div>
-                
-            </div>
-            <div className="card-total">
-                <h2>&#8377;16</h2>
-            </div>
+import food from "../utils/Images/food.jpg";
+import "../components/menu.css";
+import { useState } from "react";
+const Card = ({ item }) => {
+  const [counter, setCounter] = useState(0);
+  const [total, setTotal] = useState(0);
+ 
+  const handlePlus = () => {
+    setCounter(counter + 1);
+    setTotal(total + item.price);
+    
+  };
+  const handleMinus = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+      setTotal(total - item.price);
+      
+    }
+  };
+  console.log(total)
+  return (
+    <>
+      <div className="card-container">
+        <div className="card-img">
+          <img src={item?.img} />
         </div>
-        </>
-    )
-}
-export default Card
+        <div className="card-name-add-remove">
+          <div className="card-inner">
+            <h2>{item?.name}</h2>
+            <div className="card-add-remove">
+              <span className="minus" onClick={handleMinus}>
+                -
+              </span>
+              <input type="text" value={counter} className="card-input" />
+              <span className="plus" onClick={handlePlus}>
+                +
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="card-total">
+          <h2>&#8377;{item?.price}</h2>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Card;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,41 @@
-
 import Card from "./Card"
+var menu = [
+    {
+      id: 1,
+      name: "Hamburger",
+      price: 699,
+      img: "https://media.cnn.com/api/v1/images/stellar/prod/220428140436-04-classic-american-hamburgers.jpg?c=original"
+    },
+    {
+      id: 2,
+      name: "Pizza",
+      price: 999,
+      img: "https://static.toiimg.com/thumb/53110049.cms?width=1200&height=900"
+    },
+    {
+      id: 3,
+      name: "French Fries",
+      price: 499,
+      img: "https://www.awesomecuisine.com/wp-content/uploads/2009/05/french-fries.jpg"
+    },
+    {
+      id: 4,
+      name: "Chicken Nuggets",
+      price: 849,
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Chicken_Nuggets.jpg/800px-Chicken_Nuggets.jpg"
+    },
+    {
+      id: 5,
+      name: "Ice Cream",
+      price: 599,
+      img: "https://www.milkmaid.in/sites/default/files/2020-07/Chocolate-Ice-Cream_0.jpg"
+    }
+  ];
 const Menu = ()=>{
     return(
         <>
-       { [1,2,3,4,5].map((res)=> {return (<Card />)} )}
+       {/* { [1,2,3,4,5].map((res)=> {return (<Card />)} )} */}
+       {menu.map((item)=>{return (<Card item={item} />)})}
         </>
     )
 }
